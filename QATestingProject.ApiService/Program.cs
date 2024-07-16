ad616e34b02d170ed676aa0e0d9edfcacf46d8c1
Matija Tivanovac <matija@alphascore.hr>
2024-07-16 09:16:46 +0200

    space

--- QATestingProject.ApiService/Program.cs
+++ QATestingProject.ApiService/Program.cs
@@ -27,6 +27,7 @@ if (app.Environment.IsDevelopment())
 }
 app.UseExceptionHandler();
 
+
 var summaries = new[]
 {
     "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
